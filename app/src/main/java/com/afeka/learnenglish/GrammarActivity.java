911846336b662e724cdc1d3f5bb1d58931195ff1
Eyad94 <37661280+Eyad94@users.noreply.github.com>
2018-08-18 12:04:02 +0300

    12:02

--- app/src/main/java/com/afeka/learnenglish/GrammarActivity.java
+++ app/src/main/java/com/afeka/learnenglish/GrammarActivity.java
@@ -9,34 +9,29 @@ import android.widget.TextView;
 
 public class GrammarActivity extends AppCompatActivity {
 
-    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grammar);
-        //text = (TextView) findViewById(R.id.link_textView);
-
     }
 
+    public void browser1(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.englisch-hilfen.de/en/exercises_list/alle_grammar.htm"));
+        startActivity(browserIntent);
+    }
 
-    //"https://www.uop.edu.jo/download/research/members/oxford_guide_to_english_grammar.pdf"
-    public void browser1(View view ){
+    public void browser2(View view){
         Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.uop.edu.jo/download/research/members/oxford_guide_to_english_grammar.pdf"));
         startActivity(browserIntent);
     }
 
-    public void browser2(View view ){
+    public void browser3(View view){
         Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.talkenglish.com/grammar/grammar.aspx"));
         startActivity(browserIntent);
     }
 
-    public void browser3(View view ){
+    public void browser4(View view){
         Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://learnenglish.britishcouncil.org/en/english-grammar"));
-        startActivity(browserIntent);
-    }
-
-    public void browser4(View view ){
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.englisch-hilfen.de/en/exercises_list/alle_grammar.htm"));
         startActivity(browserIntent);
     }
 }
