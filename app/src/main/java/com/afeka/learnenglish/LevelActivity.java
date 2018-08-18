5d9f82f20c8512db87b6133f771dc72437a8a39e
Eyad94 <37661280+Eyad94@users.noreply.github.com>
2018-08-19 01:13:40 +0300

    1:15

--- app/src/main/java/com/afeka/learnenglish/LevelActivity.java
+++ app/src/main/java/com/afeka/learnenglish/LevelActivity.java
@@ -2,20 +2,12 @@ package com.afeka.learnenglish;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
 
 public class LevelActivity extends AppCompatActivity {
 
@@ -85,6 +77,11 @@ public class LevelActivity extends AppCompatActivity {
         points = sharedPreferences.getInt("POINTS",0);
         points_textView.setText(String.valueOf(points));
     }
+
+    @Override
+    public void onBackPressed() {
+    }
+
 
     private void button_level_click(String level){
         Intent OptionActivity_intent = new Intent(LevelActivity.this , OptionActivity.class);
