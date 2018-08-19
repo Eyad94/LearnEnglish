64daf2eb0d9d86b60f006444c47feebb1b6db0d9
Eyad94 <37661280+Eyad94@users.noreply.github.com>
2018-08-19 17:09:57 +0300

    17:10

---
+++ app/src/main/java/com/afeka/learnenglish/HomeActivity.java
@@ -0,0 +1,66 @@
+package com.afeka.learnenglish;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class HomeActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+    String username;
+    EditText username_input;
+    Button button_go;
+    int points;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        sharedPreferences = getSharedPreferences("UserInfo", 0);
+        username = sharedPreferences.getString("USERNAME", "");
+        points = sharedPreferences.getInt("POINTS",0);
+
+        if(username.length() > 0){
+            Intent LevelActivity_intent = new Intent(this, LevelActivity.class);
+            startActivity(LevelActivity_intent);
+        }
+
+        username_input = findViewById(R.id.name_editText);
+        button_go = findViewById(R.id.start_button);
+
+        points = 0;
+
+        button_go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button_go_clicked();
+            }
+        });
+    }
+
+
+    private void button_go_clicked(){
+
+        if (username_input.getText().length() == 0 ) {
+            Toast.makeText(this, "please enter name!!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        username = username_input.getText().toString();
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("USERNAME",username);
+        editor.putInt("POINTS",0);
+        editor.commit();
+
+        Intent LevelActivity_intent = new Intent(this, LevelActivity.class);
+        startActivity(LevelActivity_intent);
+    }
+}
--- app/src/main/java/com/afeka/learnenglish/MainActivity.java
+++ app/src/main/java/com/afeka/learnenglish/MainActivity.java
@@ -1,66 +1,30 @@
 package com.afeka.learnenglish;
 
+
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+
 
 
 public class MainActivity extends AppCompatActivity {
-
-    SharedPreferences sharedPreferences;
-    String username;
-    EditText username_input;
-    Button button_go;
-    int points;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sharedPreferences = getSharedPreferences("UserInfo", 0);
-        username = sharedPreferences.getString("USERNAME", "");
-        points = sharedPreferences.getInt("POINTS",0);
-
-        if(username.length() > 0){
-            Intent LevelActivity_intent = new Intent(this, LevelActivity.class);
-            startActivity(LevelActivity_intent);
-        }
-
-        username_input = findViewById(R.id.name_editText);
-        button_go = findViewById(R.id.start_button);
-
-        points = 0;
-
-        button_go.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-               button_go_clicked();
+            public void run() {
+                //Do this after 1000ms=1sec
+                Intent HomeActivity_intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(HomeActivity_intent);
+                MainActivity.this.finish();
             }
-        });
-    }
+        }, 3000);
 
 
-    private void button_go_clicked(){
-
-        if (username_input.getText().length() == 0 ) {
-            Toast.makeText(MainActivity.this, "please enter name!!", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        username = username_input.getText().toString();
-
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("USERNAME",username);
-        editor.putInt("POINTS",0);
-        editor.commit();
-
-        Intent LevelActivity_intent = new Intent(this, LevelActivity.class);
-        startActivity(LevelActivity_intent);
     }
 }
